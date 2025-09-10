40e9ca3bd1da26f8e1d13ea7f678565e7109e2a6
BB <23u10746@gmail.com>
2025-09-10 18:13:24 +0530

    second

--- index.js
+++ index.js
@@ -2,19 +2,20 @@ import sql from "./db.js";
 import express from "express";
 import axios from "axios";
 import cors from "cors";
+import bcrypt from "bcryptjs";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
- 
-app.get("/login" ,cors(),async (req, res) => {
+ app.use(cors());
+app.get("/login" ,async (req, res) => {
   try {
-        console.log(req.query.password, "  jbygyy ");
+        console.log(req.query, "  jbygyy ");
     const result = await sql`SELECT password,id FROM usertable WHERE email = ${req.query.email}`;
     console.log(result[0].password,"  hujhg");
- if(req.query.password == result[0].password){
+ if(bcrypt.compareSync(req.query.password,result[0].password)){
     res.status(200).json({ message: "Login successful" ,id:result[0].id});
  }
     else{
@@ -26,6 +27,20 @@ app.get("/login" ,cors(),async (req, res) => {
   } 
 });
 
+
+app.post('/signup',async (req,res)=>{
+try{
+  console.log('yoyoy yoy',req.body);
+let result=await sql `Insert into usertable(name,email,password) values (${req.body.form.name},${req.body.form.email},${req.body.form.password}) returning *`
+if(result.length>0)
+  res.json({message:"done"});
+else
+  res.json({message:"Not"});
+}catch(error){
+console.log("error:",error.message);
+res.json({message:"Internal Server Error"});
+}
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
