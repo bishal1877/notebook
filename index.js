7ab7839fa1c63f4b8c86ac7b1e374ed1c35eef5a
bishal1877 <bishalburnwal1877@gmail.com>
2025-10-08 17:27:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(
   cors({
-    origin: "http://localhost:5173", // your frontend URL
+    origin: "https://notestaker-2m8z.onrender.com",
     credentials: true,
   })
 );
