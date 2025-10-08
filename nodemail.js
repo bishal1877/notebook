8e86d075bb305553f695c033228bf724fc9aeb51
bishal1877 <bishalburnwal1877@gmail.com>
2025-10-08 21:32:07 +0530

    Update nodemail.js

--- nodemail.js
+++ nodemail.js
@@ -4,8 +4,8 @@ dotenv.config();
 
 export  const transporter = nodemailer.createTransport({
   host: process.env.SMTPS,
-  port: 465,
-  secure: true, // true for 465, false for other ports
+  port: 587,
+  secure: false, // true for 465, false for other ports
   auth:{
 user:process.env.Login,
 pass:process.env.Pass,
