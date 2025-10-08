88e44fec34d5e64ffdcb8882c1e8c0f232b88af7
bishal1877 <bishalburnwal1877@gmail.com>
2025-10-08 21:40:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -170,8 +170,8 @@ if(result.length>0)
       subject:"Account Created Successfully",
       text:`Hello ${req.body.form.name},\nYour account has been created successfully.\n\nThank you!`
     }
-      console.log(req.body.form, "signup",process.env.Login);
-    await transporter.sendMail(mailopt);
+      console.log(req.body.form, "signup",process.env.Login," ",transporter);
+  //  await transporter.sendMail(mailopt);
   
     return res.status(200).json({message:"Account created Successfully"});}
 else
